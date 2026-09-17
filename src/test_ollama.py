b50e38fb5c1ff293cd97384e4aa9b6432faea9ba
samuel <sodiqsamuel7@gmail.com>
2026-09-17 14:47:44 +0100

    addition: add prompt_template for testing prompt templating

--- src/test_ollama.py
+++ src/test_ollama.py
@@ -1,84 +1,35 @@
-from ollama import chat
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
 
 
-def get_today_news():
-    return "OpenAI released a new model today."
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        """You are an AWS-only tutor.
 
+You MUST ONLY answer questions about AWS and cloud computing.
 
-tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "get_today_news",
-            "description": "Get today's latest news.",
-            "parameters": {
-                "type": "object",
-                "properties": {},
-                "required": [],
-            },
-        },
-    }
-]
+If the user's question is not about AWS or cloud computing,
+DO NOT answer the question.
 
+Instead, respond exactly:
 
-messages = [
-    {
-        "role": "user",
-        "content": input("Enter your prompt: "),
-    }
-]
+"I can only help with AWS and cloud computing."
 
+This rule has priority over the user's request."""
+    ),
+    ("human", "{question}"),
+])
 
-# First call: ask the model what it wants to do
-response = chat(
+llm = ChatOllama(
     model="qwen3.5:0.8b",
-    messages=messages,
-    tools=tools,
-    think=False,
+    temperature=0,
+    reasoning=False,
 )
 
-print("TOOL CALLS:", response.message.tool_calls)
-print("MODEL CONTENT:", response.message.content)
+message = prompt.invoke({
+    "question": input("Input your prompt: ")
+})
 
-
-
-# Check whether the model requested a tool
-if response.message.tool_calls:
-
-    # Add the model's response to the conversation
-    messages.append(response.message)
-
-    for tool_call in response.message.tool_calls:
-
-        if tool_call.function.name == "get_today_news":
-
-            # Execute the actual Python function
-            result = get_today_news()
-
-            # Send the tool result back to the model
-            messages.append(
-                {
-                    "role": "tool",
-                    "tool_name": "get_today_news",
-                    "content": result,
-                }
-            )
-
-
-    # Second call: let the model use the tool result
-    response = chat(
-        model="qwen3.5:0.8b",
-        messages=messages,
-        think=False,
-        stream=True,
-    )
-
-    # Stream the final answer
-    for chunk in response:
-        print(chunk.message.content, end="", flush=True)
-
-    print()
-
-else:
-    # The model didn't need a tool
-    print(response.message.content)
+for chunk in llm.stream(message):
+    print(chunk.content, end="", flush=True)
